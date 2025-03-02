a447cb9eac72b48424bc7e97e7ade883c01599a9
vaidehi2401 <dholevaidehi2@gmail.com>
2025-03-02 13:16:01 +0530

    Encrypted Password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
+
 const sequelize = require('../util/database');
+const bcrypt = require('bcrypt');
 exports.signup= async(req, res)=>{
 try{
  const name = req.body.user.name;
@@ -7,8 +9,10 @@ try{
  if(!name || !email || !password){
     return res.status(400).json({ error: "All fields are required" });
  }
+ const saltRounds = 10;
+ const hashedPassword = await bcrypt.hash(password, saltRounds);
  await sequelize.query("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", {
-    replacements: [name, email, password], 
+    replacements: [name, email, hashedPassword], 
 });
 res.status(200).json({ message: "User added successfully"});
 }
@@ -34,11 +38,18 @@ exports.login= async(req, res)=>{
       if (!emailExist) {
         return res.status(404).json({ error: "User not found" });
       }
-      const validPassword =  (password===emailExist.password);
-      if (!validPassword) {
-        return res.status(401).json({ error: "Invalid password" });
+      bcrypt.compare(password, emailExist.password , (err, result)=>{
+        if(err){
+          throw new Error("Something went wrong")
+        }
+        else if(result===true){
+          res.status(200).json({ message: "Login successful", user: emailExist });
+        }
+        else{
+          return res.status(401).json({ error: "Invalid password" });
+        }
       }
-      res.status(200).json({ message: "Login successful", user: emailExist });
+    ) 
     }
     catch (error) {
         console.error("Database error:", error);
